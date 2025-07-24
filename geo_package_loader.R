80cd211cf44ddaaf3cb93c2f7f54db3fa3e06ba4
Tom Belanger <spltt.tlb@gmail.com>
2025-07-24 16:24:54 +0200

    Initial commit

---
+++ geo_package_loader.R
@@ -0,0 +1,21 @@
+# This script loads all packages associated with the project using renv.
+
+# R/packages.R
+
+# List of required packages
+packages <- c(
+    "tidyverse",
+    "tidymodels",
+    "usethis"
+)
+
+# Install missing packages using renv
+missing <- packages[!packages %in% installed.packages()[,"Package"]]
+
+if (length(missing) > 0) {
+    message("Installing missing packages: ", paste(missing, collapse = ", "))
+    renv::install(missing)
+}
+
+# Load all packages
+invisible(lapply(packages, library, character.only = TRUE))
