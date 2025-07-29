5ee783cba195ff89259da007274cc3a037a80414
Tom Belanger <spltt.tlb@gmail.com>
2025-07-30 00:38:44 +0200

    Updated texas grid tiler and added graphical checker

--- texas_grid_tiler.R
+++ texas_grid_tiler.R
@@ -5,7 +5,7 @@ options(tigris_use_cache = TRUE)
 # --- Parameters ---
 tile_size <- 1       # degrees
 overlap <- 0.1       # degrees
-timeout_sec <- 120    # seconds
+timeout_sec <- 180    # seconds
 key <- "building"
 value <- c("residential", "house", "apartments")
 cache_dir <- "osm_cache"
@@ -205,3 +205,41 @@ while (i <= length(tile_list)) {
 }
 
 message("Saved ", length(final_tiles), " successfully processed tiles.")
+
+#The following code uses leaflet to check the configuration of the successful tiles
+
+# Load your final tiles (assuming it's the saved RDS list)
+final_tiles <- readRDS("final_tiles.rds")
+#Remove all non-successful tiles
+
+j <- 1
+while(j <= length(final_tiles)) {
+    
+    if(final_tiles[[j]]$status != "Success") {
+        
+        final_tiles <- final_tiles[-j]
+        j <- j - 1
+        
+    }
+    
+    j <- j + 1
+    
+}
+
+# Extract geometries and flatten into an sfc object
+tile_sfc <- do.call(c, lapply(final_tiles, function(x) x$geometry))
+
+# Convert to sf object
+tile_sf <- st_sf(geometry = tile_sfc)
+
+# Plot with leaflet
+leaflet(tile_sf) %>%
+    addProviderTiles("CartoDB.Positron") %>%
+    addPolygons(
+        color = "black",        # border color
+        weight = 1,             # border thickness
+        fillColor = "blue",     # tile fill color
+        fillOpacity = 0.5,      # transparency (0=transparent, 1=opaque)
+        opacity = 1             # border opacity
+    )
+
