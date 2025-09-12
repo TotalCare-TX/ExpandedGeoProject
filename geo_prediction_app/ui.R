46217c7d63d0d0657453ec4dd606e7a45707b949
Eddy-Zaher <e.elzaher@totalcare.us>
2025-09-12 20:16:31 +0300

    Steps 1 & 2

--- geo_prediction_app/ui.R
+++ geo_prediction_app/ui.R
@@ -1,39 +1,72 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(bslib)
+library(leaflet)
 
-
-# Define UI for application that draws a histogram
-fluidPage(
-
+shinyUI(
+  fluidPage(
+    # ---- CSS to make map taller ----
+    tags$head(
+      tags$style(HTML("
+        #prediction_map {
+          height: 90vh !important;  
+          width: 100% !important;   
+        }
+      "))
+    ),
+    
     navset_pill(
-        
-        nav_panel("Map Prediction",
-                  wellPanel("Instructions for using the map"),
-                  
-                  wellPanel(
-                      leafletOutput("prediction_map", height = 600)
+      # -------------------
+      # TAB 1: Map Prediction
+      # -------------------
+      nav_panel("Map Prediction",
+                wellPanel(
+                  h4("📍 Instructions: Map Prediction"),
+                  tags$ol(
+                    tags$li("Click anywhere on the map to select a location."),
+                    tags$li("The latitude/longitude of your click will be displayed below."),
+                    tags$li("These coordinates will also be sent automatically to the Site Prediction tab.")
                   )
-                  ),
-        
-        nav_panel("Site Prediction",
-                  wellPanel("Instructions for using site prediction"),
-                  
-                  wellPanel("Address Entry"),
-                  
-                  wellPanel("Results display")
-                  
+                ),
+                leafletOutput("prediction_map"),
+                wellPanel(
+                  h5("Selected Coordinates:"),
+                  textOutput("clicked_coords")
+                )
+      ),
+      
+      # -------------------
+      # TAB 2: Site Prediction
+      # -------------------
+      nav_panel("Site Prediction",
+                wellPanel(
+                  h4("📍 Instructions: Site Prediction"),
+                  tags$ol(
+                    tags$li("Coordinates from the map will auto-fill below if you clicked on the map."),
+                    tags$li("Or enter an address and click 'Geocode Address' to get coordinates."),
+                    tags$li("Click 'Train Model' to load the model using `training_data.csv`."),
+                    tags$li("Click 'Predict' to generate a prediction using the chosen coordinates (or manual X1/X2 values).")
                   )
-        
+                ),
+                
+                # Inputs for model
+                wellPanel(
+                  textInput("address", "Enter Address:", ""),
+                  actionButton("geocode_btn", "Geocode Address"),
+                  textOutput("coords"),
+                  tags$hr(),
+                  actionButton("train_model", "Train Model"),
+                  textInput("x1", "Enter X1 (lat):"),
+                  textInput("x2", "Enter X2 (lon):"),
+                  actionButton("predict_button", "Predict")
+                ),
+                
+                # Outputs
+                wellPanel(
+                  textOutput("result"),
+                  tableOutput("history_table")
+                )
+      )
     ),
     id = "tab"
-    
+  )
 )
